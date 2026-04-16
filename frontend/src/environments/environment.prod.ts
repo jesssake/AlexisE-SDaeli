59f48035cea4aa07bf7bad7dfe7b4147188e9e04
Jessica Ake <125330130+jesssake@users.noreply.github.com>
2026-04-15 21:13:29 -0600

    Delete frontend directory
    Signed-off-by: Jessica Ake <125330130+jesssake@users.noreply.github.com>

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-
-  // En servidor ya estás dentro de /gestion_e
-  apiUrl: '/gestion_e',
-
-  // 🔥 API BASE para producción
-  apiBase: '/gestion_e'
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-
-  // API LOCAL
-  apiUrl: 'http://localhost/gestion_e',
-
-  // 🔥 API BASE COMPATIBLE con TODOS TUS MÓDULOS
-  apiBase: 'http://localhost/gestion_e'
-};
